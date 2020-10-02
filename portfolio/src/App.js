8cda6684a09ea9384ac00e189ec566a90a48af41
matt-rdsglobal <52655311+matt-rdsglobal@users.noreply.github.com>
2020-10-02 18:18:10 +0100

    Add icons and extra pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
 
 function App() {
   return (
@@ -20,6 +21,43 @@ function App() {
         </a>
       </header>
     </div>
+=======
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './contents/Home';
+import About from './contents/About';
+import Education from './contents/Education';
+import Skills from './contents/Skills';
+import Contact from './contents/Contact';
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/education">
+          <Education />
+        </Route>
+        <Route path="/skills">
+          <Skills />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </div>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
