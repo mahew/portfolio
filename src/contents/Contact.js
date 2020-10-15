975474308814199b36090ad2cc78ebb7db50cfaa
Matthew <N0743797@my.ntu.ac.uk>
2020-10-15 16:27:15 +0100

    Update Icons, Update Navbar, Update Content

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -5,8 +5,8 @@ class Contact extends Component {
         return (
             <div className="condiv">
                 <h1 className="subtopic">Contact Me</h1>
-                <h3>Email  :   naafi96@gmail.com</h3>
-                <h3>Instagram   :   @iam_naafi</h3>
+                <h3>I'd love to get in touch! I prefer a twitter DM but also feel free to email me if you'd like ❤</h3>
+                <h1><a className="mail" href="mailto:matt@matthew-ball.com">matt@matthew-ball.com</a></h1>
                 <Social />
             </div>
         )
